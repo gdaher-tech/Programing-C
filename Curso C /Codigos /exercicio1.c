c21c50bd895ea44dd6380acd1858c48fca53e99a
gdaher-tech <gustavo2000daher@gmail.com>
2025-03-15 03:49:30 -0300

    inite commit

---
+++ Curso C /Codigos /exercicio1.c
@@ -0,0 +1,15 @@
+// FACA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO E O IMPRIMA NA TELA 
+
+#include <stdio.h> 
+
+int main(){
+
+    int numero; 
+
+    printf("Insira um número inteiro: "); 
+    scanf("%d", &numero);
+    
+    printf("O número inserido pelo usuário foi %d", numero);
+
+
+}
