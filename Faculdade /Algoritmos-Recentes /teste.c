2a5b14cfd0d4d1b834cb1cc1ff929dbe39a4cda8
gdaher-tech <gustavo2000daher@gmail.com>
2025-03-15 03:42:38 -0300

    init informations

--- Faculdade /Algoritmos-Recentes /teste.c
+++ Faculdade /Algoritmos-Recentes /teste.c
@@ -1,8 +1,5 @@
-float ap1_2, ap2_2, ap3_2, np_2, av2;
+#include <stdio.h> 
 
-
-calculo_atividades_praticas_1(float ap1, float ap2, float ap3){
-    return atividades_praticas = ((ap1 + ap2 + ap3 + np_1) / 3) * 0.3; 
+int main(){
+    printf("Fé que vai dar certo"); 
 }
-
-printf()
