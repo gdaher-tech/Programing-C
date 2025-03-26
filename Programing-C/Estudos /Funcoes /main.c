abee987323ba1df7218d79483b0ca8abce3ea6d6
gdaher-tech <gustavo2000daher@gmail.com>
2025-03-26 15:59:39 -0300

    Adicionar pastas 'estudos' e 'funções' com novos arquivos

---
+++ Programing-C/Estudos /Funcoes /main.c
@@ -0,0 +1,8 @@
+//
+//  main.c
+//  Programing-C
+//
+//  Created by Gustavo Daher  on 26/03/25.
+//
+
+#include "main.h"
---
+++ Programing-C/Estudos /Funcoes /main.h
@@ -0,0 +1,13 @@
+//
+//  main.h
+//  Programing-C
+//
+//  Created by Gustavo Daher  on 26/03/25.
+//
+
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+#endif /* main_h */
